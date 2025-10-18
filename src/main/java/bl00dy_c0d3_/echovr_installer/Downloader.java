a9688ea1798bfc7fea90e6db7bce2dcca3ff36de
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2025-10-18 19:15:58 +0200

    Update download server URL in Downloader.java

--- src/main/java/bl00dy_c0d3_/echovr_installer/Downloader.java
+++ src/main/java/bl00dy_c0d3_/echovr_installer/Downloader.java
@@ -229,7 +229,7 @@ public class Downloader implements Runnable {
     // Choose the fastest available http server
     private static String getDownloadSpeed(JLabel labelProgress, JDialog frame) {
         String[] servers = new String[2];
-        servers[0] = "https://echo.marceldomain.de:6969/";
+        servers[0] = "https://echo.mia-hentschel.de/";
         servers[1] = "https://evr.echo.taxi/";
         String testFile = "randomDownloadTestFile";
 
